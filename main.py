6c88208797616120e42798947387bd06bc9093b8
Supriya <supriya.rani@hotmail.com>
2021-12-04 15:38:42 +0530

    commit

---
+++ main.py
@@ -0,0 +1,18 @@
+import numpy as np
+import re
+import torch
+from torch.utils.data import TensorDataset, random_split
+#from transformers import AdamW
+#import time
+#import tensorflow as tf
+#tf.config.experimental.list_physical_devices('GPU')
+
+class news_qa:
+    def __init__(self, text="Hi"):
+        self.doc_text = text
+
+
+if __name__ == '__main__':
+
+    news_qa = news_qa()
+    print("It's working!")
